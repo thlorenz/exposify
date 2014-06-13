69c973963af2160fbdf02e88d338c76f52f374bc
Nico Rehwaldt <git_nikku@nixis.de>
2014-06-13 14:56:56 +0200

    feat(transform): allow pass options via browserify transform
    This commit adds support for passing the exposify options via
    
    ```js
    var b = browserify();
    
    // setting via transform argument
    b.transform({ expose: { jquery: '$', three: 'THREE' } }, 'exposify');
    ```
    
    BREAKING CHANGE:
    
    * Requires browserify >= 3.4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var transformify = require('transformify')
   , through = require('through2')
   , expose = require('./expose');
 
-exports = module.exports = 
+exports = module.exports =
 
 /**
  * browserify transform which exposes globals as modules that can be required.
@@ -12,18 +12,24 @@ exports = module.exports =
  * @name exposify
  * @function
  * @param {string} file file whose content is to be transformed
+ * @param {Object} options (exposify config)
  * @return {TransformStream} transform that replaces require statements found in the code with global assigments
  */
-function exposify(file) {
- if (!exports.filePattern.test(file)) return through();  
+function exposify(file, opts) {
+  opts = opts && Object.keys(opts).length ? opts : {
+    filePattern: exports.filePattern,
+    expose: exports.config
+  };
 
- if (typeof exports.config !== 'object') {
-   throw new Error('Please set exposify.config or $EXPOSIFY_CONFIG so it knows what to expose');
- }
+  if (opts.filePattern && !opts.filePattern.test(file)) return through();
 
- var tx = transformify(expose.bind(null, exports.config));
- return tx(file);
-}
+  if (typeof opts.expose !== 'object') {
+   throw new Error('Please pass { expose: { ... } } to transform, set exposify.config or $EXPOSIFY_CONFIG so it knows what to expose');
+  }
+
+  var tx = transformify(expose.bind(null, opts.expose));
+  return tx(file);
+};
 
 
 /**
@@ -31,6 +37,13 @@ function exposify(file) {
  * You need to set this or provide it via the `EXPOSIFY_CONFIG` environment variable.
  *
  * ### Example
+ *
+ *  ```js
+ *  var b = browserify();
+ *
+ *  // setting via transform argument
+ *  b.transform({ expose: { jquery: '$', three: 'THREE' } }, 'exposify');
+ *  ```
  *
  *  ```js
  *  // setting from javascript
@@ -41,9 +54,9 @@ function exposify(file) {
  *  # setting from command line
  *  EXPOSIFY_CONFIG='{ "jquery": "$", "three": "THREE" }' browserify -t exposify ...
  *  ```
- * 
+ *
  * @name exposify::config
- * 
+ *
  */
 exports.config = (function () {
   if (process.env.EXPOSIFY_CONFIG) {
@@ -59,14 +72,14 @@ exports.config = (function () {
 
 /**
  * Regex pattern of files whose content is exposified
- * 
+ *
  * @name exposify::filePattern
  */
 exports.filePattern = /\.js$/;
 
 /**
  * Exposes the expose function that operates on a string
- * 
+ *
  * @name exposify::expose
  */
 exports.expose = expose;
