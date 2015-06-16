9cac1cf38602f8b6c6dc15db8191e8fb3b91f465
Ben Drucker <bvdrucker@gmail.com>
2015-06-16 00:49:24 -0400

    Restore the global-compatible replacement from 0.2
    Closes thlorenz/browserify-shim#175

--- expose.js
+++ expose.js
@@ -2,10 +2,11 @@
 
 var mapObject = require('map-obj');
 var replaceRequires = require('replace-requires');
+var globalize = require('globo')
 
 module.exports = function expose (replacements, code) {
-  replacements = mapObject(replacements, function (id, globalVar) {
-    return [id, '(window.' + globalVar + ')']
+  replacements = mapObject(replacements, function (moduleId, globalId) {
+    return [moduleId, globalize(globalId)]
   })
   return replaceRequires(code, replacements)
 }
